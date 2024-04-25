e304e91c6ace56204e2931d62141dfc62e725bf7
Belinda Caylor <belinda.caylor@gmail.com>
2024-04-25 10:24:46 -0500

    Add axois promise to general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,6 +3,8 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios');
+const URL = 'https://belindacaylo-5000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai';
 
 public_users.post("/register", (req,res) => {
     const user = req.body.user;
@@ -30,27 +32,50 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  return res.status(200).send(JSON.stringify(books,null,4));
+    axios.get(URL)
+        .then(resp => {
+            return res.status(200).send(resp.data);
+        })
+        .catch(err => {
+            return res.status(500).json({ error: 'Internal server error'});
+        });
 });
+
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
-    return res.status(200).send(JSON.stringify(books[isbn],null,4));
+    axios.get(URL + '/isbn/' + isbn)
+        .then(resp => {
+            return res.status(200).send(resp.data);
+        })
+        .catch(err => {
+            return res.status(500).json({ error: 'Internal server error'});
+        });
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     const author = req.params.author;
-    const filteredBooks = Object.values(books).filter(book => book.author === author);
-    return res.status(200).send(JSON.stringify(filteredBooks,null,4));
+    axios.get(URL + '/author/' + author)
+        .then(resp => {
+            return res.status(200).send(resp.data);
+        })
+        .catch(err => {
+            return res.status(500).json({ error: 'Internal server error'});
+        });
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     const title = req.params.title;
-    const filteredBooks = Object.values(books).filter(book => book.title === title);
-    return res.status(200).send(JSON.stringify(filteredBooks,null,4));
+    axios.get(URL + '/title/' + title)
+        .then(resp => {
+            return res.status(200).send(resp.data);
+        })
+        .catch(err => {
+            return res.status(500).json({ error: 'Internal server error'});
+        });
 });
 
 //  Get book review
